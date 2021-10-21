6eac4e7e5e8135ba20fa58fd018bf9327e27957a
johnstarr-ling <71774021+johnstarr-ling@users.noreply.github.com>
2021-10-21 18:03:12 -0400

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,14 +4,14 @@ if [[ $TRAVIS_BRANCH == 'site' ]] ; then
   cd _site
   git init
 
-  git config user.name "Travis CI"
-  git config user.email "rohitjain18005@gmail.com"
+  git config user.name "John R. Starr"
+  git config user.email "johnstarr1997@gmail.com"
   git add .
   git commit -m "Deploy"
 
   # We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  git push --force --quiet "https://${GH_TOKEN}@github.com/rohitjain00/rohitjain00.github.io.git" master:master > /dev/null 2>&1
+  git push --force --quiet "https://${GH_TOKEN}@github.com/johnstarr-ling/johnstarr-ling.github.io.git" master:master > /dev/null 2>&1
 else
   echo 'Invalid branch. You can only deploy from site.'
   exit 1
